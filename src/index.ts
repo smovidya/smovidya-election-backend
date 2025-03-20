3a6840ee14cf023dd70cbdee98b9c1b9d4cce3e3
Patsagorn <patsagorn_y@outlook.com>
2025-03-21 02:02:14 +0700

    feat: integrate OpenAPI support in election routes and enhance vote submission schema

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,31 @@
-import { Hono } from "hono";
+import { OpenAPIHono } from "@hono/zod-openapi";
 import { swaggerUI } from "@hono/swagger-ui";
+import { apiReference } from "@scalar/hono-api-reference";
 
 // Routes
 import { electionRoutes } from "./routes/election.route";
 
-const app = new Hono<{ Bindings: Env }>();
+const app = new OpenAPIHono<{ Bindings: Env }>();
 
-app.route("/api", electionRoutes).get("/spec", swaggerUI({ url: "/api" }));
+app
+	.route("/api", electionRoutes)
+	.doc("/spec.json", {
+		openapi: "3.0.0",
+		info: {
+			version: "v1",
+			title: "SMO Vidya Election API",
+			contact: {
+				name: "SMO Vidya Election Backend Team",
+				url: "https://github.com/smovidya/smovidya-election-backend",
+			},
+		},
+	})
+	.get("/swagger", swaggerUI({ url: "/spec.json" }))
+	.get(
+		"/reference",
+		apiReference({
+			url: "/spec.json",
+		}),
+	);
 
 export default app;
--- src/routes/election.route.ts
+++ src/routes/election.route.ts
@@ -1,7 +1,40 @@
 import { Hono } from "hono";
 import { electionController } from "../controllers/election.controller";
+import { describeRoute } from "hono-openapi";
+import { createRoute, OpenAPIHono } from "@hono/zod-openapi";
+import {
+	submitVoteResponseSchema,
+	submitVoteSchema,
+} from "../schemas/election.schema";
 
-export const electionRoutes = new Hono<{ Bindings: Env }>()
-	.get("/candidates", electionController.getCandidates)
-	.post("/vote", electionController.submitVote)
-	.get("/results", electionController.getResults);
+// export const electionRoutes = new Hono<{ Bindings: Env }>()
+// 	.post("/vote", electionController.submitVote)
+// 	.get("/candidates", electionController.getCandidates)
+// 	.get("/results", electionController.getResults);
+
+export const electionRoutes = new OpenAPIHono<{ Bindings: Env }>().openapi(
+	createRoute({
+		method: "post",
+		path: "/vote",
+		request: {
+			params: submitVoteSchema,
+		},
+		responses: {
+			200: {
+				content: {
+					"application/json": {
+						schema: submitVoteResponseSchema,
+					},
+				},
+				description: "Success",
+			},
+			400: {
+				description: "Bad Request",
+			},
+			401: {
+				description: "Unauthorized",
+			},
+		},
+	}),
+	electionController.submitVote,
+);

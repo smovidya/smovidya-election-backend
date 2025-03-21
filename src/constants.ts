fb7add678d960e380afb68d8961aefc79d730202
Patsagorn <patsagorn_y@outlook.com>
2025-03-22 02:07:33 +0700

    feat: add election date constants for vote scheduling and result announcement

---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+// TODO: Change the election date and time to the actual election date and time.
+
+export const electionInfo = {
+	// The start date and time of the election.
+	voteStart: new Date("2025-04-25T00:00:00+07:00"),
+	// The end date and time of the election.
+	voteEnd: new Date("2025-04-26T00:00:00+07:00"),
+	// Announcement date and time of the election results.
+	resultAnnouncement: new Date("2025-04-27T00:00:00+07:00"),
+};

ae4347709dd610d5166a062e47bfd14d86a9f02e
Patsagorn <patsagorn_y@outlook.com>
2025-03-21 11:28:29 +0700

    feat: update

--- src/models/election.model.ts
+++ src/models/election.model.ts
@@ -1,15 +1,19 @@
-import type { Vote } from '../schemas/election.schema';
-import type { ControllerContext } from '../utils/types';
+import type { Vote } from "../schemas/election.schema";
+import type { ControllerContext } from "../utils/types";
 
 // TODO: Implement when database is available
 export const electionModel = {
-  addVotes: async (c: ControllerContext, { voterId, votes }: { voterId: string, votes: Vote[] }) => {
-    const voteStatements = votes.map((vote) => {
-      return c.env.DB.prepare(
-        `INSERT INTO votes (voter_id, candidate_id, position) VALUES (?, ?, ?)`,
-      ).bind(voterId, vote.candidateId, vote.position);
-    });
+	addVotes: async (
+		c: ControllerContext,
+		{ voterId, votes }: { voterId: string; votes: Vote[] },
+	) => {
+		const voteStatements = votes.map((vote) => {
+			// if voter is already voted, update
+			return c.env.DB.prepare(
+				"INSERT INTO votes (voter_id, candidate_id, position) VALUES (?, ?, ?)",
+			).bind(voterId, vote.candidateId, vote.position);
+		});
 
-    const result = await c.env.DB.batch(voteStatements);
-  }
-}
+		const result = await c.env.DB.batch(voteStatements);
+	},
+};

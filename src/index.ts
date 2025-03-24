f928bcef0d078a314f71316d786bba0a4ea54264
Patsagorn <patsagorn_y@outlook.com>
2025-03-24 15:17:21 +0700

    style: format

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import { env } from "cloudflare:workers";
 import { Elysia } from "elysia";
 
 const app = new Elysia()
-  .get("/", "Hello Elysia")
-  .get("/user/:id", ({ params: { id } }) => id)
-  .post("/form", ({ body }) => body);
+	.get("/", "Hello Elysia")
+	.get("/user/:id", ({ params: { id } }) => id)
+	.post("/form", ({ body }) => body);
 
 export default app;

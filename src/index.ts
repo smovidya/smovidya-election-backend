47a665b41ad7eaf40977741409136411a4375998
Patsagorn <patsagorn_y@outlook.com>
2025-03-24 13:46:35 +0700

    feat: add JWT bearer authentication to OpenAPI specification

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,18 @@ import { electionRoutes } from "./routes/election.route";
 const app = new OpenAPIHono<{ Bindings: Env }>();
 
 app
+	.openAPIRegistry.registerComponent('securitySchemes', 'Bearer', {
+		type: 'http',
+		scheme: 'bearer',
+		bearerFormat: "JWT",
+		description: "Google ID Token from Firebase Authentication",
+		in: "header",
+		name: "Authorization",
+	})
+
+app
 	.route("/api", electionRoutes)
-	.doc("/spec.json", {
+	.doc("/spec.json", (c) => ({
 		openapi: "3.0.0",
 		info: {
 			version: "v1",
@@ -21,7 +31,18 @@ app
 				url: "https://github.com/smovidya/smovidya-election-backend",
 			},
 		},
-	})
+		security: [{ bearerAuth: [] }],
+		servers: [
+			{
+				url: new URL(c.req.url).origin,
+				description: 'Current environment',
+			},
+			{
+				url: "https://api-smovidya-election.bunyawatapp37204.workers.dev/",
+				description: "Production environment",
+			}
+		],
+	}))
 	.get("/swagger", swaggerUI({ url: "/spec.json" }))
 	.get(
 		"/reference",
